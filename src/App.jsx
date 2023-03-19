9134aea79376b7c9c839e0f011d7809ad75f2c32
ruamazi <119816587+ruamazi@users.noreply.github.com>
2023-03-19 19:37:56 +0000

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import { Link } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
